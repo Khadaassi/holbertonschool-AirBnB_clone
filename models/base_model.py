4bcc4b9122dc3e599c3d00535a80a1b73d78d925
Khadaassi <aassikhadija@gmail.com>
2024-03-05 16:51:46 +0100

    Update import statement in base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@
 
 import uuid
 import datetime
-import models
+from models import storage
 
 class BaseModel:
     """Base class for AirBnB clone"""
@@ -22,7 +22,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+            storage.new(self)
 
     def __str__(self):
         """Return string representation of BaseModel instance"""
@@ -31,7 +31,7 @@ class BaseModel:
     def save(self):
         """Update updated_at attribute with current datetime"""
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.save()
     
     def to_dict(self):
         """Return dictionary representation of BaseModel instance"""
