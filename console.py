967ac41df93acf204bc08bda6282f4c9e60e12e9
Khadaassi <aassikhadija@gmail.com>
2024-03-06 18:29:48 +0100

    Add command interpreter functionality

--- console.py
+++ console.py
@@ -1,9 +1,28 @@
 #!/usr/bin/python3
+
+"""This module contains the entry point of the command interpreter"""
+
 import cmd
 
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
-if __name__ == '__main__':
+    def do_quit(self, arg):
+        """Exit the program"""
+        print("Exiting...")
+        return True
+
+    def do_EOF(self, arg):
+        """Exit the program"""
+        print("Exiting...")
+        return True
+
+    def default(self, line):
+        if line.strip() == "":
+            return
+        print(f"Command not recognized: {line}")
+
+
+if __name__ == "__main__":
     HBNBCommand().cmdloop()
