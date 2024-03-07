13dfe3f28148de02c54b6385381441fd50b1c0b1
Khadaassi <aassikhadija@gmail.com>
2024-03-07 10:13:27 +0100

    Add model imports and update class dictionary

--- models/__init__.py
+++ models/__init__.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
 """"""
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 f_storage = FileStorage()
 f_storage.reload()
 
-classes = [
-    "BaseModel",
-    "User",
-    "State",
-    "Place",
-    "City",
-    "Amenity",
-    "Review"
-]
+classes = {
+    "BaseModel": BaseModel,
+    "User": User,
+    "State": State,
+    "Place": Place,
+    "City": City,
+    "Amenity": Amenity,
+    "Review": Review
+    }
--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,6 @@
 
 import uuid
 from datetime import datetime
-from models import f_storage
 
 class BaseModel:
     """Base class for AirBnB clone"""
@@ -22,6 +21,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            from models import f_storage
             f_storage.new(self)
 
     def __str__(self):
@@ -31,6 +31,7 @@ class BaseModel:
     def save(self):
         """Update updated_at attribute with current datetime"""
         self.updated_at = datetime.now()
+        from models import f_storage
         f_storage.new(self)
         f_storage.save()
     
