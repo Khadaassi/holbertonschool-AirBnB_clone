a97e29999ce4c8f5ebe5171c3736d3ac9b4848ea
DDoudine <amandinekemp@gmail.com>
2024-03-07 15:27:44 +0100

    Add tests for Place class default values

---
+++ tests/test_models/test_place.py
@@ -0,0 +1,18 @@
+import unittest
+from models.place import Place
+
+
+class test_place(unittest.TestCase):
+    def test_place(self):
+        """Tests default values for Place class attributes"""
+        self.assertEqual(Place.city_id, "")
+        self.assertEqual(Place.user_id, "")
+        self.assertEqual(Place.name, "")
+        self.assertEqual(Place.description, "")
+        self.assertEqual(Place.number_rooms, 0)
+        self.assertEqual(Place.number_bathrooms, 0)
+        self.assertEqual(Place.max_guest, 0)
+        self.assertEqual(Place.price_by_night, 0)
+        self.assertEqual(Place.latitude, 0.0)
+        self.assertEqual(Place.longitude, 0.0)
+        self.assertEqual(Place.amenity_ids, [])
