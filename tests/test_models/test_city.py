1477ae655931e6907ed79b06583f0d63dbbbac22
DDoudine <amandinekemp@gmail.com>
2024-03-07 14:24:46 +0100

    Add tests for City

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,9 @@
+import unittest
+from models.city import City
+
+
+class test_city(unittest.TestCase):
+    def test_city(self):
+        """Tests default values for City class attributes"""
+        self.assertEqual(City.name, "")
+        self.assertEqual(City.state_id, "")
