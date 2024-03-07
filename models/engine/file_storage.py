462124e2fd17e81d0e2441dc0e4aefc675885cd5
Khadaassi <aassikhadija@gmail.com>
2024-03-07 21:42:32 +0100

    Refactor file_storage.py to use string formatting and class variables

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -25,27 +25,27 @@ class FileStorage:
 
     def new(self, obj):
         """Sets in __objects the obj with key <obj class name>.id"""
-        key = obj.__class__.__name__ + "." + obj.id
+        key = "{}.{}".format(obj.__class__.__name__, getattr(obj, "id"))
         FileStorage.__objects[key] = obj
 
     def save(self):
         """Serializes __objects to the JSON file"""
         serialized_objects = {}
-        for key, obj in self.__objects.items():
+        for key, obj in FileStorage.__objects.items():
             serialized_objects[key] = obj.to_dict()
-        
-        with open(self.__file_path, 'w') as file:
+
+        with open(FileStorage.__file_path, "w") as file:
             json.dump(serialized_objects, file)
 
     def reload(self):
         """Deserializes the JSON file to __objects"""
         try:
-            with open(self.__file_path, 'r') as file:
+            with open(FileStorage.__file_path, "r") as file:
                 loaded_objects = json.load(file)
                 for key, value in loaded_objects.items():
-                    class_name, obj_id = key.split('.')
+                    class_name, obj_id = key.split(".")
                     class_ = eval(class_name)
                     instance = class_(**value)
-                    self.__objects[key] = instance
+                    FileStorage.__objects[key] = instance
         except FileNotFoundError:
             pass
