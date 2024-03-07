dc4f8d5ea5dc80b73afb96b32d84afafda449efd
Khadaassi <aassikhadija@gmail.com>
2024-03-07 16:20:54 +0100

    pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -10,12 +10,8 @@ class BaseModel:
     """Base class for AirBnB clone"""
 
     def __init__(self, *args, **kwargs):
-        """Initialization instance attributes
-        Args:
-             args: set of arguments
-             kwargs: set of arguments with keywords
-        """
-
+        """Initialize class instance"""
+        
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
@@ -27,34 +23,21 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = self.created_at
             from models import f_storage
-
             f_storage.new(self)
 
     def __str__(self):
-        """
-        String representation of instance
-        Returns:
-            str: string representation of instance
-        """
+        """Return string representation of BaseModel instance"""
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """
-        Updated the public instance attribute
-        updated_at : with the current datetime.
-        """
+        """Update updated_at attribute with current datetime"""
         self.updated_at = datetime.now()
         from models import f_storage
-
         f_storage.new(self)
         f_storage.save()
 
     def to_dict(self):
-        """
-        Returning instance dictionnary
-        Returns:
-            dict: Instance dictionary
-        """
+        """Return dictionary representation of BaseModel instance"""
         new_dict = self.__dict__.copy()
         new_dict["__class__"] = self.__class__.__name__
         new_dict["created_at"] = self.created_at.isoformat()
