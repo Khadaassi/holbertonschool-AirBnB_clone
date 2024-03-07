3dfd3df32e3a935682f8ff1693695d42402f4326
Khadaassi <aassikhadija@gmail.com>
2024-03-07 21:27:30 +0100

    Refactor save and reload methods in FileStorage class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -30,15 +30,22 @@ class FileStorage:
 
     def save(self):
         """Serializes __objects to the JSON file"""
-        with open(FileStorage.__file_path, "w") as f:
-            json.dump({k: v.to_dict() for k, v in FileStorage.__objects.items()}, f)
+        serialized_objects = {}
+        for key, obj in self.__objects.items():
+            serialized_objects[key] = obj.to_dict()
+        
+        with open(self.__file_path, 'w') as file:
+            json.dump(serialized_objects, file)
 
     def reload(self):
         """Deserializes the JSON file to __objects"""
-        if os.path.exists(FileStorage.__file_path):
-            with open(FileStorage.__file_path, "r") as f:
-                objs = json.load(f)
-            for obj in objs.values():
-                cls = obj["__class__"]
-                if cls in globals():
-                    FileStorage.__objects[obj["id"]] = globals()[cls](**obj)
+        try:
+            with open(self.__file_path, 'r') as file:
+                loaded_objects = json.load(file)
+                for key, value in loaded_objects.items():
+                    class_name, obj_id = key.split('.')
+                    class_ = eval(class_name)
+                    instance = class_(**value)
+                    self.__objects[key] = instance
+        except FileNotFoundError:
+            pass
