81e0fecb40a614b9d5406442c2b626b1a9fc014c
DDoudine <amandinekemp@gmail.com>
2024-03-07 14:24:17 +0100

    Add tests for Amenity

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Performs unit tests for the Amenity class """
+
+import unittest
+from models.base_model import BaseModel
+from models.amenity import Amenity
+
+
+class TestAmenity(unittest.TestCase):
+    def setUp(self):
+        """Set up for the tests"""
+        self.amenity = Amenity()
+
+    def test_name_default(self):
+        """Test the default name attribute"""
+        self.assertEqual(self.amenity.name, "")
+
+    def test_name_type(self):
+        """Test the type of name"""
+        self.assertIsInstance(self.amenity.name, str)
